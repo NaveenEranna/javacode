dcb3078ec916dd14b8ef38e06b3b3a61baa1f0bd
ErannaVitalPaper <eranna@vitalpaper.com>
2023-04-15 11:59:59 +0530

    first commit

---
+++ src/main/java/com/javaproject/javaaws/JavaawsApplication.java
@@ -0,0 +1,13 @@
+package com.javaproject.javaaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaproject/javaaws/JavaawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.javaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
